d7b2c352f7c967904ace867fbe1d34c43ee7b2f2
theblackcat102 <zhirui09400@gmail.com>
2021-01-01 18:04:54 +0800

    bump sub revision

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ ext_modules = [
 
 setup(
     name='extractnet',
-    version='1.0.2',
+    version='1.0.3',
     description='Extract the main article content (and optionally comments) from a web page',
     author='Ray',
     author_email='zhirui.tam@currentsapi.services',
