f02c54d85de9b7f542dcdba1389d9deb85db8243
theblackcat102 <theblackcat102@github.com>
2022-10-28 22:02:44 +0800

    add test case for url parsing

--- test/test_integration.py
+++ test/test_integration.py
@@ -28,3 +28,8 @@ def test_other_case():
         'https://oil.in-en.com/html/oil-2945141.shtml',
         'https://www.msn.com/zh-tw/money/topstories/%25E5%258F%25B0%25E5%258C%2597%25E6%259C%2580%25E6%2596%25B0%25E6%2588%25BF%25E5%2583%25B9%25E6%258C%2587%25E6%2595%25B8%25E6%259B%259D%25E5%2585%2589-%25E3%2580%258C%25E9%2580%2599%25E7%2594%25A2%25E5%2593%2581%25E3%2580%258D%25E9%2580%25A32%25E6%259C%2588%25E4%25B8%258B%25E6%25BB%2591%25E6%259C%2588%25E7%25B7%259A%25E3%2580%2581%25E5%25AD%25A3%25E7%25B7%259A%25E9%2583%25BD%25E8%25BD%2589%25E6%258A%2598/ar-AA12IMQH',
     ]
+    extractor = Extractor()
+    for url in urls:
+        res = requests.get(url)
+        web = res.content.decode('utf-8')
+        extractor(web, metadata_mining=True)
--- test/test_metadata.py
+++ test/test_metadata.py
@@ -1,7 +1,9 @@
 import io
 import os
+from re import L
 from shutil import rmtree
 import tempfile
+from wsgiref import validate
 
 import pytest
 from extractnet.metadata_extraction.metadata import extract_metadata
@@ -16,3 +18,28 @@ def test_meta_extraction():
     assert results['title'] != None
     assert results['author'] != None
     assert results['video'] != None
+
+
+
+from extractnet.metadata_extraction.url_utils import validate_date
+
+
+def test_date_validate_from_url():
+
+    from datetime import datetime
+    urls = [
+        ('https://www.msn.com/en-gb/news/newsbirmingham/man-taken-to-hospital-with-burns-after-lithium-battery-explodes-in-great-barr-house/ar-AA12JUmH', datetime(2022, 1, 1)),
+        ('https://www.washingtontimes.com/news/2022/oct/27/lucianne-goldberg-bill-clinton-impeachment-figure-/', datetime(2022, 10, 27)),
+        ('https://www.azcentral.com/picture-gallery/news/local/arizona/2017/10/18/remembering-arizona-leaders-who-have-died/106769086/', datetime(2017, 10, 18)),
+        ('https://www.cnn.com/2017/01/23/politics/cdc-climate-conference-canceled-trump-administration/index.html', datetime(2017, 1, 23)),
+        ('https://www.cnn.com/2017/05/29/tennis/french-open-tennis-djokovic-agassi-nadal/index.html', datetime(2017, 5, 29)),
+        ('https://dfw.cbslocal.com/2015/01/police-officers-give-sick-boy-a-b-day-surprise/', datetime(2015, 1, 1)),
+        ('https://www.cnn.com/2022/2/29/politics/cdc-climate-conference-canceled-trump-administration/index.html', datetime(2022, 2, 28)),
+        ('http://www.apnewsarchive.com/2015/Even-70-years-later-Allied-firebombing-of-Dresden-still-fresh-in-survivor-s-mind/id-1862c9192bdc46289e303f2c443eb13b', datetime(2015, 1, 1)),
+        ('http://www.china.org.cn/world/Off_the_Wire/2022-10/08/content_78455811.htm', datetime(2022, 10, 8)),
+    ]
+
+    for (url, target_date) in urls:
+        fixed_date = validate_date(url, datetime(2022, 1, 1, 12, 11, 10))
+        assert fixed_date.date() == target_date.date()
+
