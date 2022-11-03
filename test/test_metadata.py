8847cdb83d93faee3e40ec4115f2937222404503
theblackcat102 <theblackcat102@github.com>
2022-11-03 20:48:46 +0800

    add url test case

--- test/test_metadata.py
+++ test/test_metadata.py
@@ -27,7 +27,10 @@ from extractnet.metadata_extraction.url_utils import validate_date
 def test_date_validate_from_url():
 
     from datetime import datetime
+    default_date = datetime(2022, 1, 1, 12, 11, 10)
     urls = [
+        ('http://rssfeeds.usatoday.com/~/718271584/0/usatodaycomsports-topstories~New-body-camera-footage-shows-Hope-Solos-DWI-arrest-from-March/',default_date),
+        ('http://rssfeeds.pnj.com/~/718260410/0/pensacola/news~Anglers-and-pedestrians-delighted-that-Palafox-Pier-has-reopened-in-Pensacola-PHOTOS/', default_date),
         ('https://www.msn.com/en-gb/news/newsbirmingham/man-taken-to-hospital-with-burns-after-lithium-battery-explodes-in-great-barr-house/ar-AA12JUmH', datetime(2022, 1, 1)),
         ('https://www.washingtontimes.com/news/2022/oct/27/lucianne-goldberg-bill-clinton-impeachment-figure-/', datetime(2022, 10, 27)),
         ('https://www.azcentral.com/picture-gallery/news/local/arizona/2017/10/18/remembering-arizona-leaders-who-have-died/106769086/', datetime(2017, 10, 18)),
@@ -40,6 +43,6 @@ def test_date_validate_from_url():
     ]
 
     for (url, target_date) in urls:
-        fixed_date = validate_date(url, datetime(2022, 1, 1, 12, 11, 10))
+        fixed_date = validate_date(url, default_date)
         assert fixed_date.date() == target_date.date()
 
